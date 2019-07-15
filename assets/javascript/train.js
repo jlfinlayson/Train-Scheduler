8cf0ef837233def38c2192c8846061571d61f26a
jess <jlfinlayson4@gmail.com>
2019-07-15 16:36:54 -0400

    "firebase added to train.js"

---
+++ assets/javascript/train.js
@@ -0,0 +1,13 @@
+
+// Linking Firebase
+var firebaseConfig = {
+        apiKey: "AIzaSyAIT13oXrhTVJCwfFnqEOnTUsBKS7zxAmY",
+        authDomain: "jess-test-7ce10.firebaseapp.com",
+        databaseURL: "https://jess-test-7ce10.firebaseio.com",
+        projectId: "jess-test-7ce10",
+        storageBucket: "jess-test-7ce10.appspot.com",
+        messagingSenderId: "374587731069",
+        appId: "1:374587731069:web:c8ef28ca494d6232"
+      };
+    
+firebase.initializeApp(firebaseConfig);
